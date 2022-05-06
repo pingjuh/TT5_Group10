8b35037c7af81875445e528547e9a83f78df5c12
nicc222 <github2@mail.nicholascheow.com>
2022-05-06 15:57:53 +0800

    fixed async issue for projects table

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -8,6 +8,8 @@ import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
 import { useTable, useSortBy } from 'react-table'
 import MaterialTable from 'material-table'
 import axios from 'axios';
+import ProjectTable from "./ProjectTable";
+
 
 function Projects() {
     const navigate = useNavigate();
@@ -15,7 +17,7 @@ function Projects() {
 
     async function getProjects(){
         axios
-        .post(`${process.env.REACT_APP_BACKEND_URL}/projects/allProjects`, {id:1}
+        .post(`${process.env.REACT_APP_BACKEND_URL}/projects/allProjects`, {id:localStorage.getItem("id")}
         )
         .then((response) => {
           console.log(response.data)
@@ -33,57 +35,6 @@ function Projects() {
         getProjects()
     }, [])
 
-    const data = React.useMemo(
-        () => [
-            {
-                "_id": "627494e853da3ab5227c3803",
-                "id": 1,
-                "name": "RTF",
-                "budget": 12000,
-                "description": "Realtime Face Recogniton"
-            }
-        ],
-        []
-      )
-
-      const columns = React.useMemo(
-        () => [
-          {
-            Header: 'ID',
-            accessor: 'id', // accessor is the "key" in the data
-          },
-          {
-            Header: 'Project Name',
-            accessor: 'name',
-          },
-          {
-            Header: 'Budget',
-            accessor: 'budget',
-          },
-          {
-            Header: 'Description',
-            accessor: 'description',
-          },{
-            Header: 'Action',
-            Cell: ({ cell }) => (
-                <button value="View Project"
-                onClick={()=>{navigate('/expenses/1')}}
-                >
-                  View Project
-                </button>
-              )
-          },
-        ],
-        []
-      )
-      const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-      } = useTable({ columns, projectData })
-    
 
 
 //   useEffect(() => {
@@ -94,50 +45,7 @@ function Projects() {
   <>
     <Heading sx={{pl:10,pt:10, pb:10}}>Projects Page</Heading>
     <Box sx={{m:15,maxWidth:"70%"}}>
-    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-       <thead>
-         {headerGroups.map(headerGroup => (
-           <tr {...headerGroup.getHeaderGroupProps()}>
-             {headerGroup.headers.map(column => (
-               <th
-                 {...column.getHeaderProps()}
-                 style={{
-                   borderBottom: 'solid 3px red',
-                   background: 'aliceblue',
-                   color: 'black',
-                   fontWeight: 'bold',
-                 }}
-               >
-                 {column.render('Header')}
-               </th>
-             ))}
-           </tr>
-         ))}
-       </thead>
-       <tbody {...getTableBodyProps()}>
-         {rows.map(row => {
-           prepareRow(row)
-           return (
-             <tr {...row.getRowProps()}>
-               {row.cells.map(cell => {
-                 return (
-                   <td
-                     {...cell.getCellProps()}
-                     style={{
-                       padding: '10px',
-                       border: 'solid 1px gray',
-                       background: 'papayawhip',
-                     }}
-                   >
-                     {cell.render('Cell')}
-                   </td>
-                 )
-               })}
-             </tr>
-           )
-         })}
-       </tbody>
-     </table>
+    {projectData && <ProjectTable data={projectData}/>}
  
     </Box>
 
