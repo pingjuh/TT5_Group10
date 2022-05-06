d6c026b09f1b5e58242abd88fc3014973762525f
nicc222 <github2@mail.nicholascheow.com>
2022-05-06 11:18:44 +0800

    Implemented login route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const User =  require('../models/Users')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  res.send('auth route');
+});
+
+router.post('/login', async function(req, res, next) {
+    const filter={username: req.body.username, password: req.body.password}
+    try{
+        const users = await User.findOne(filter)
+        return res.send(users)
+    }catch (err) {
+        console.log(err);
+        return res.status(400).json({
+          error: true,
+          message: "Error",
+        });
+      }
+    
+    
+  });
+
+  router.get('/addUser', async function(req, res, next) {
+      const user = new User({
+          username:"personone",
+          password: "person1password",
+          name: "Person one name",
+          appointment: "staff"
+
+      })
+      try{
+          await user.save()
+          return res.sendStatus(200)
+      }catch (err) {
+        console.log(err);
+        return res.status(400).json({
+          error: true,
+          message: "Failed to save",
+        });
+      }
+    res.sendStatus(200);
 });
 
 module.exports = router;
+
