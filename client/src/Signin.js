cb3b8096cca85a8038d89c010f55347485908ad4
coderboyz17 <honlixian17@gmail.com>
2022-05-06 12:50:40 +0800

    Update Signin.js

--- client/src/Signin.js
+++ client/src/Signin.js
@@ -1,1 +1,32 @@
+import { useRef, useState, useEffect, useContext } from "react"
+import { Link } from 'react-router-dom';
 
+const SIGNIN_URL = '/signin'
+
+const Signin = () => {
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pw, setPw] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pw])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user, pwd);
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
+
+}
