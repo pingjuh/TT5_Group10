7fba8350b3ca6c4076767f9a4b193b97c6a6777f
coderboyz17 <honlixian17@gmail.com>
2022-05-06 16:25:12 +0800

    Add EditExpense

---
+++ client/src/EditExpense.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const EditExpense = () => {
+  return (
+    <div>
+        <div>
+            Edit Expense
+        </div>
+        <Button>
+            Edit
+        </Button>
+        
+        <Button>
+            Delete
+        </Button>
+    </div>
+  )
+}
+
+export default EditExpense
