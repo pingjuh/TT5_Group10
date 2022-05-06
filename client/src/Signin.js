c68524201bc5f30095784034b1eda60d56f36731
coderboyz17 <honlixian17@gmail.com>
2022-05-06 12:53:22 +0800

    Update Signin.js

--- client/src/Signin.js
+++ client/src/Signin.js
@@ -29,4 +29,53 @@ const Signin = () => {
         setSuccess(true);
     }
 
+    return (
+        <>
+            {success ? (
+                <section>
+                    <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <Link to= "/home">Go to Home</Link>
+                    </p>
+                </section>
+            ) : (
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <hi>Sign In</hi>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
+                <input 
+                    type ="text"
+                    id="username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required 
+                />
+
+<label htmlFor="password">Password:</label>
+                <input 
+                    type ="password"
+                    id="password"
+                    onChange={(e) => setPw(e.target.value)}
+                    value={pw}
+                    required 
+                />
+
+                <button>Sign In</button>
+            </form>
+            <p>
+                        New User?<br />
+                        <span className="line">
+                                <Link to= "/register">Sign Up</Link>
+                        </span>
+                    </p>
+        </section>
+            )}
+        </>
+  )
 }
+
+export default Signin
